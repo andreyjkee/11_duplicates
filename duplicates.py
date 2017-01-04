7a3bbe89c312be77d386ffa8eba4257a7b0ef3a3
andrey.selemenev@rtlabs.ru <andrey.selemenev@rtlabs.ru>
2017-01-04 14:50:57 +0300

    using argparse and speedup

--- duplicates.py
+++ duplicates.py
@@ -1,37 +1,27 @@
+import argparse
 import os
-from optparse import OptionParser
+from collections import defaultdict
 
-
-def are_files_duplicates(file_path_1, file_path_2):
-    if not os.path.exists(file_path_1):
-        raise FileNotFoundError(file_path_1)
-    if not os.path.exists(file_path_2):
-        raise FileNotFoundError(file_path_2)
-    return all([os.path.getsize(file_path_1) == os.path.getsize(file_path_2),
-                os.path.basename(file_path_1) == os.path.basename(file_path_2)])
 
 def get_filepaths(directory):
     file_paths = []
+    name_size_to_paths = defaultdict(list)
     for root, directories, files in os.walk(directory):
         for filename in files:
             filepath = os.path.join(root, filename)
             if os.path.exists(filepath):
+                name_size_to_paths[os.path.basename(filepath) + ' ' + str(os.path.getsize(filepath))].append(filepath)
                 file_paths.append(filepath)
-    return file_paths
+    return name_size_to_paths
 
 
 if __name__ == '__main__':
-    parser = OptionParser()
-    parser.add_option('-d', '--dir', type='string', dest='directory')
-    (options, args) = parser.parse_args()
-    duplicates = []
-    all_files = get_filepaths(options.directory)
-    list_of_pairs = [(all_files[p1], all_files[p2])
-                     for p1 in range(len(all_files)) for p2 in range(p1 + 1, len(all_files))]
-    for pair in list_of_pairs:
-        if are_files_duplicates(*pair):
-            duplicates.append(pair)
+    parser = argparse.ArgumentParser(description="Get duplicated files")
+    parser.add_argument("-d", "--dir", type=str, dest="directory", required=True)
+    options = parser.parse_args()
+    all_files_dict = get_filepaths(options.directory)
+    duplicates = [val for val in all_files_dict.values() if len(val) > 1]
     if duplicates:
-        print('Found duplicated files:')
-        for dupe in duplicates:
-            print('File 1: {0}, file 2: {1}'.format(dupe[0], dupe[1]))
+        print('Found duplicated files (by name and size):')
+        for paths in duplicates:
+            print(paths)
